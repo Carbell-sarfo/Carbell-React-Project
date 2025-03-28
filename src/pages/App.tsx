c5128cadbaa6ab00f7e146ea79b19e0f95fcbf23
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2025-03-28 13:38:04 +0000

    second form trial

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,6 @@
 import Accordion from "../components/accordion.js";
 import Form from "../components/Form.tsx";
+import Form2 from "../components/Form-2.tsx";
 
 function App() {
   return (
@@ -9,6 +10,9 @@ function App() {
 
       {/* Form */}
       <Form />
+
+      {/* Form 2 */}
+      <Form2 />
     </>
   );
 }
